af0d519a051a14f564ee3b9a30ed3957848bef65
Joey <laozhao1005@gmail.com>
2016-07-29 22:51:59 +0800

    更新到最新代码

--- src/com/kit/baidumap/LocationUtils.java
+++ src/com/kit/baidumap/LocationUtils.java
@@ -1,7 +1,5 @@
 package com.kit.baidumap;
 
-import android.content.Context;
-
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
@@ -9,33 +7,36 @@ import com.baidu.location.LocationClientOption.LocationMode;
 
 public class LocationUtils {
 
-	public static void startLocation(Context context,
-			LocationClient locationClient,String coorType, BDLocationListener bdLocationListener) {
+    public static void startLocation(LocationClient locationClient
+            , String coorType, BDLocationListener bdLocationListener) {
 
-		locationClient.registerLocationListener(bdLocationListener); // 注册监听函数
-		// 定位初始化
-		LocationClientOption option = new LocationClientOption();
-		option.setLocationMode(LocationMode.Hight_Accuracy);// 设置定位模式
-		option.setCoorType(coorType);// 返回的定位结果是百度经纬度,默认值gcj02
-		// option.setScanSpan(HiBitConfig.baiduMapScanSpan);//
-		// 设置发起定位请求的间隔时间为5000ms
-		option.setIsNeedAddress(true);// 返回的定位结果包含地址信息
-		option.setNeedDeviceDirect(true);// 返回的定位结果包含手机机头的方向
-		option.setOpenGps(true);
-		locationClient.setLocOption(option);
-		locationClient.start();
+        if (locationClient == null) {
+            return;
+        }
 
-		// TODO Auto-generated method stub
-		if (locationClient != null && locationClient.isStarted()) {
-			locationClient.requestLocation();
-		}
+        locationClient.registerLocationListener(bdLocationListener); // 注册监听函数
+        // 定位初始化
+        LocationClientOption option = new LocationClientOption();
+        option.setLocationMode(LocationMode.Hight_Accuracy);// 设置定位模式
+        option.setCoorType(coorType);// 返回的定位结果是百度经纬度,默认值gcj02
+        // option.setScanSpan(HiBitConfig.baiduMapScanSpan);//
+        // 设置发起定位请求的间隔时间为5000ms
+        option.setIsNeedAddress(true);// 返回的定位结果包含地址信息
+        option.setNeedDeviceDirect(true);// 返回的定位结果包含手机机头的方向
+        option.setOpenGps(true);
+        locationClient.setLocOption(option);
+        locationClient.start();
 
-	}
+        if (locationClient.isStarted()) {
+            locationClient.requestLocation();
+        }
 
-	// How to use?
+    }
+
+    // How to use?
 
 	/*
-	 * LocationUtils.startLocation(mContext, new LocationClient(
+     * LocationUtils.startLocation(mContext, new LocationClient(
 	 * getApplicationContext()), new BDLocationListener() {
 	 * 
 	 * @Override public void onReceiveLocation(BDLocation location) {
--- src/com/kit/extend/gps/AddressTask.java
+++ src/com/kit/extend/gps/AddressTask.java
@@ -1,58 +1,58 @@
-package com.kit.extend.gps;
-
-import android.app.Activity;
-import android.database.Cursor;
-import android.net.Uri;
-
-import org.apache.http.HttpHost;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.params.ConnRouteParams;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.HttpConnectionParams;
-import org.json.JSONObject;
-
-
-public class AddressTask extends IAddressTask {
-
-	public AddressTask(Activity context, int postType) {
-		super(context, postType);
-	}
-
-	@Override
-	public HttpResponse execute(JSONObject params) throws Exception {
-		HttpClient httpClient = new DefaultHttpClient();
-
-		HttpConnectionParams.setConnectionTimeout(httpClient.getParams(),
-				20 * 1000);
-		HttpConnectionParams.setSoTimeout(httpClient.getParams(), 20 * 1000);
-
-		HttpPost post = new HttpPost("http://74.125.71.147/loc/json");
-		// ���ô���
-		if (postType == DO_APN) {
-			Uri uri = Uri.parse("content://telephony/carriers/preferapn"); // ��ȡ��ǰ����ʹ�õ�APN�����
-			Cursor mCursor = context.getContentResolver().query(uri, null,
-					null, null, null);
-			if (mCursor != null) {
-//				mCursor.moveToNext(); // �α�������һ����¼����ȻҲֻ��һ��
-				if(mCursor.moveToFirst()) {
-					String proxyStr = mCursor.getString(mCursor
-							.getColumnIndex("proxy"));
-					if (proxyStr != null && proxyStr.trim().length() > 0) {
-						HttpHost proxy = new HttpHost(proxyStr, 80);
-						httpClient.getParams().setParameter(
-								ConnRouteParams.DEFAULT_PROXY, proxy);
-					}
-				}
-			}
-		}
-		
-		StringEntity se = new StringEntity(params.toString());
-		post.setEntity(se);
-		HttpResponse response = httpClient.execute(post);
-		return response;
-	}
-
-}
+//package com.kit.extend.gps;
+//
+//import android.app.Activity;
+//import android.database.Cursor;
+//import android.net.Uri;
+//
+//import org.apache.http.HttpHost;
+//import org.apache.http.HttpResponse;
+//import org.apache.http.client.HttpClient;
+//import org.apache.http.client.methods.HttpPost;
+//import org.apache.http.conn.params.ConnRouteParams;
+//import org.apache.http.entity.StringEntity;
+//import org.apache.http.impl.client.DefaultHttpClient;
+//import org.apache.http.params.HttpConnectionParams;
+//import org.json.JSONObject;
+//
+//
+//public class AddressTask extends IAddressTask {
+//
+//	public AddressTask(Activity context, int postType) {
+//		super(context, postType);
+//	}
+//
+//	@Override
+//	public HttpResponse execute(JSONObject params) throws Exception {
+//		HttpClient httpClient = new DefaultHttpClient();
+//
+//		HttpConnectionParams.setConnectionTimeout(httpClient.getParams(),
+//				20 * 1000);
+//		HttpConnectionParams.setSoTimeout(httpClient.getParams(), 20 * 1000);
+//
+//		HttpPost post = new HttpPost("http://74.125.71.147/loc/json");
+//		// ���ô���
+//		if (postType == DO_APN) {
+//			Uri uri = Uri.parse("content://telephony/carriers/preferapn"); // ��ȡ��ǰ����ʹ�õ�APN�����
+//			Cursor mCursor = context.getContentResolver().query(uri, null,
+//					null, null, null);
+//			if (mCursor != null) {
+////				mCursor.moveToNext(); // �α�������һ����¼����ȻҲֻ��һ��
+//				if(mCursor.moveToFirst()) {
+//					String proxyStr = mCursor.getString(mCursor
+//							.getColumnIndex("proxy"));
+//					if (proxyStr != null && proxyStr.trim().length() > 0) {
+//						HttpHost proxy = new HttpHost(proxyStr, 80);
+//						httpClient.getParams().setParameter(
+//								ConnRouteParams.DEFAULT_PROXY, proxy);
+//					}
+//				}
+//			}
+//		}
+//
+//		StringEntity se = new StringEntity(params.toString());
+//		post.setEntity(se);
+//		HttpResponse response = httpClient.execute(post);
+//		return response;
+//	}
+//
+//}
